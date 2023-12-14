aa907dbb7e6191e6996681cd4c29160243605e44
baraiyakishan2011 <142770390+baraiyakishan2011@users.noreply.github.com>
2023-12-14 17:44:05 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+main(){
+	int pi=3.14;
+	int r=20;
+	int circle=pi*r*r;
+	printf("area of circle = %d \n",circle);
+	
+	int l=49;
+	int square=l*l;
+	printf("area of square = %d \n",square);
+	
+	int w=20;
+	int rectangle=l*w;
+	printf("area of rectangle = %d \n",rectangle);
+	
+	int h=40;
+	int b=17;
+	int triangle=h*b/2;
+	printf("area of triangle = %d \n",triangle);
+	
+	int p=500;
+	int n=7;
+	int interest=p*r*n/100;
+	printf("area of interest = %d \n",interest);
+	
+	int a=2;
+	int Perimeter_of_circle=2*pi*r;
+	printf("Perimeter_of_circle = %d \n",Perimeter_of_circle);
+}
